d7588c9449f06c86cf97b6b04c606955076f8210
MaartenGr <maarten_grootendorst@hotmail.com>
2020-04-29 21:06:51 +0200

    Add PPO pretrained

--- test.py
+++ test.py
@@ -5,8 +5,11 @@ from TheGame.Models import DQN, D3QN, PERD3QN, DRQN, PPO, PERDQN
 #                DQN(152, load_model="Brains/DQN/model_40000_881.pt"),           # <-- CURRENTLY BEST BRAIN!!!
 #                D3QN(152, 8, load_model="Brains/D3QN/model_60000_100.pt")]      # <-- CURRENTLY BEST BRAIN!!!
 
-main_brains = [PERD3QN(153, 8, load_model="Experiments/Best Brain/PERD3QN/brain_gene_1.pt", training=False)
-               for _ in range(8)]
+# main_brains = [PERD3QN(153, 8, load_model="Experiments/Best Brain/PERD3QN/brain_gene_1.pt", training=False)
+#                for _ in range(8)]
+main_brains = [PPO(153, 8, load_model="C:/Users/Maarten/Downloads/brain_gene_0 (1).pt")
+               for _ in range(2)]
+
 
 tester(main_brains, width=30, height=30, max_agents=150, pastel_colors=False, static_families=True,
        limit_reproduction=False, fps=10)
